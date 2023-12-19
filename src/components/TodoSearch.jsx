4d67391538c1c96152355659d47fc073524e5d52
Alex <jcotamomoli@uniminuto.edu.co>
2023-12-18 23:46:22 -0500

    create todo

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,13 +1,16 @@
 import './TodoSearch.css'
+import { useState } from 'react'; // import useState
+
 
 export const TodoSearch = () => {
+  const [search, setSearch] = useState('') // declare a state variable called search and initialize it to an empty string
+  console.log(search)
   return (
     <>
       <div className="todo-search-container">
         <div className="todo-text">
-        <input  className='todo-placeholder' type="text" placeholder="search..." onChange={(e)=>{
-          console.log("searching...")
-          console.log(e.target.value)
+        <input  className='todo-placeholder' type="text" placeholder="search..." value={search} onChange={(e)=>{
+          setSearch(e.target.value)
         }} />
 
 
